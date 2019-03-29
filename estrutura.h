0dfe93d179acd344599e2f2ac9adbac97c30942f
Manuel Martins <37194324+manel27@users.noreply.github.com>
2019-03-29 20:30:35 +0000

    Adicionados novos campos na struct Elem

--- estrutura.h
+++ estrutura.h
@@ -2,15 +2,26 @@ typedef enum {ATRIB, ADD, SUB, MUL, IF_I, PRINT, READ, GOTO_I, LABEL} OpKind;
 
 typedef enum {EMPTY, INT_CONST, STRING} ElemKind;
 
-#define
+//#define
 #define NXT(P)((P)->nxt)
 
 typedef struct {
   ElemKind kind;
   union{
-    int val;
-    char *name;
-  } contents;
+   struct {
+      int val;
+      char *name;
+    }int;
+    struct {
+      int *p;
+      char *name;
+    }label;
+    struct {
+      char *content;
+      char *name;
+    }string; 
+  }contents;
+  
 } Elem;
 
 typedef struct{
