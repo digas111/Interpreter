4a4f42e67f684428f635106eab6b013d60f2f16e
Manel <armando.m.martins@hotmail.com>
2019-04-02 15:57:52 +0100

    A ver se é desta

--- main.c
+++ main.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include "estrutura.h"
-=======
 #include "hash.h"
-
->>>>>>> f61171a8650f1d8747cd83911e56cc9e3c388210
-
 #define DEBUGMODE 1
 
-<<<<<<< HEAD
 int main(){
-=======
-  lookup("boas",2,1);
+
+  if(DEBUGMODE){
+
+    lookup("boas",2,1);
   lookup("comandos",3,1);
   lookup("cenas",1,1);
   lookup("coisas",4,1);
   lookup("sjjs",5,1);
 
   print();
->>>>>>> f61171a8650f1d8747cd83911e56cc9e3c388210
-
-  if(DEBUGMODE){
     NODE *lista = NULL;
 
   Elem *e1= new_elem_int(INT_CONST, 1, "name");
@@ -29,7 +22,6 @@ int main(){
 
   Instr *instru = new_instr(ADD, e1, e2, e3);
 
-<<<<<<< HEAD
   lista=new_node(instru, lista);
   lista=new_node(instru, lista);
   lista=new_node(instru, lista);
@@ -44,6 +36,3 @@ int main(){
   return 0;
   }
 }
-=======
-}
->>>>>>> f61171a8650f1d8747cd83911e56cc9e3c388210
