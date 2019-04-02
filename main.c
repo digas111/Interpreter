307acc012c9c5f11d2f9bdffd54981a983d03deb
Manel <armando.m.martins@hotmail.com>
2019-04-02 16:24:41 +0100

    problemas de git resolved

--- main.c
+++ main.c
@@ -5,34 +5,34 @@
 
 int main(){
 
-  if(DEBUGMODE){
+  if (DEBUGMODE){
 
-    lookup("boas",2,1);
-  lookup("comandos",3,1);
-  lookup("cenas",1,1);
-  lookup("coisas",4,1);
-  lookup("sjjs",5,1);
+    lookup("boas", 2, 1);
+    lookup("comandos", 3, 1);
+    lookup("cenas", 1, 1);
+    lookup("coisas", 4, 1);
+    lookup("sjjs", 5, 1);
 
-  print();
+    print();
     NODE *lista = NULL;
 
-  Elem *e1= new_elem_int(INT_CONST, 1, "name");
-  Elem *e2= new_elem_int(INT_CONST, 1, "name");
-  Elem *e3= new_elem_int(INT_CONST, 1, "name");
+    Elem *e1 = new_elem_int(INT_CONST, 1, "name");
+    Elem *e2 = new_elem_int(INT_CONST, 1, "name");
+    Elem *e3 = new_elem_int(INT_CONST, 1, "name");
 
-  Instr *instru = new_instr(ADD, e1, e2, e3);
+    Instr *instru = new_instr(ADD, e1, e2, e3);
 
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
-  lista=new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
+    lista = new_node(instru, lista);
 
-  int n = length(lista);
-  printf("%d\n", n);
+    int n = length(lista);
+    printf("%d\n", n);
 
-  return 0;
+    return 0;
   }
 }
